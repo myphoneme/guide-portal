89100240cc2a97a4bbc77798ae0a2caa06ba1a87
Ronik Vats <ronikvats@gmail.com>
2025-02-24 17:34:23 +0530

    Bloglist

--- src/BlogList/BlogList.jsx
+++ src/BlogList/BlogList.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import styles from "./BlogList.module.css"; 
+import React, { useEffect, useState } from 'react';
+import styles from "./BlogList.module.css";
 import { Button, Card, Container, Row, Col, Nav } from 'react-bootstrap';
 
 function BlogList() {
-  return (
-    <div>
-          <Container className={styles.storyContainer}>
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("http://fastapi.phoneme.in/posts")
+            .then(response => response.json())
+            .then(data => {
+                setPosts(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching posts:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <Container className={styles.storyContainer}>
             <div className={styles.storyHeading}>
-              <div>
-              <h3 >Your Story</h3>
-              </div>
-              <div className={styles.storyButtonGroup}>
-                <Button className={styles.storyWriteButton}>Write a Story</Button>
-                <Button className={styles.storyImportButton}>Import a Story</Button>
-              </div>
+                <h3>Your Story</h3>
+                <div className={styles.storyButtonGroup}>
+                    <Button className={styles.storyWriteButton}>Write a Story</Button>
+                    <Button className={styles.storyImportButton}>Import a Story</Button>
+                </div>
             </div>
             <Nav defaultActiveKey="#published" className={styles.storyNavTabs}>
                 <Nav.Item>
@@ -23,145 +36,34 @@ function BlogList() {
                     <Nav.Link eventKey="#published" className={styles.storyPublishedTab}>Published</Nav.Link>
                 </Nav.Item>
             </Nav>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://softwarelogic.net/wp-content/uploads/2017/07/technology-wallpaper-40.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
-            <Card className={styles.storyCard}>
-                <Row>
-                    <Col xs={2} md={2} className={styles.storyImageCol}>
-                        <img src="http://cdn.wallpapersafari.com/94/68/lOsTXu.jpg" alt="Story" className={styles.storyImage} />
-                    </Col>
-                    <Col>
-                        <h5 className={styles.storyTitle}>Lorem Ipsum has been the industry's</h5>
-                        <p className={styles.storyContent}>It to make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into make a type specimen book. It has survived not only five essentially also the leap into essentially also the centuries, but also the leap into electronic typesetting, remaining essentially also the leap into</p>
-                        <small className={styles.storyUpdatedText}>UPDATED: 2 MONTHS AGO</small>
-                        <small className={styles.storyPublishedText}>PUBLISHED: 2 MONTHS AGO</small>
-                        <div className={styles.storyButtonGroupBottom}>
-                            <Button className={styles.storyEditButton}>Edit</Button>
-                            <Button className={styles.storyDeleteButton}>Delete</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Card>
+            {loading ? (
+                <p>Loading posts...</p>
+            ) : posts.length > 0 ? (
+                posts.map(post => (
+                    <Card key={post.id} className={styles.storyCard}>
+                        <Row>
+                            <Col xs={2} md={2} className={styles.storyImageCol}>
+                                {/* <img src={"http://fastapi.phoneme.in/."post.image || "static/images/health.jpeg"} alt="Story" className={styles.storyImage} /> */}
+                                <img src={`http://fastapi.phoneme.in/${post.image}`} alt="Story" className={styles.storyImage} />
+                            </Col>
+                            <Col>
+                                <h5 className={styles.storyTitle}>{post.title}</h5>
+                                <p className={styles.storyContent}>{post.post}</p>
+                                <small className={styles.storyUpdatedText}>Created Time: {post.created_at || "Unknown"}</small>
+                                <small className={styles.storyPublishedText}>Author: {post.created_user.name || "Unknown"}</small>
+                                <div className={styles.storyButtonGroupBottom}>
+                                    <Button className={styles.storyEditButton}>Edit</Button>
+                                    <Button className={styles.storyDeleteButton}>Delete</Button>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card>
+                ))
+            ) : (
+                <p>No posts available.</p>
+            )}
         </Container>
-    </div>
-  )
+    );
 }
 
-export default BlogList
+export default BlogList;
--- src/CreateBlog/CreateBlog.jsx
+++ src/CreateBlog/CreateBlog.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import { Button } from "react-bootstrap";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function CreateBlog() {
   return (
-    <div>CreateBlog</div>
+    <div>
+        <h2>Create Blog</h2>
+        <form>
+        <div>
+          <label>
+            <b>Category:</b>
+          </label>
+          <select
+            id="category"
+            name="category"
+            required
+          >
+            <option value="">Select Category</option>
+          </select>
+
+        
+          <label>
+            <b>Post Title:</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Post Title"
+            name="post"
+            required
+          />
+          
+        </div>
+         
+          <label>
+            <b>Body:</b>
+          </label>
+          <ReactQuill
+            theme="snow"
+            placeholder="Enter Post Description"
+            required
+          />
+         
+
+          <div>
+          <label>
+            <b>Upload Image:</b>
+          </label>
+          <input
+            type="file"
+            name="image"
+            accept="image/*"
+            required
+          />
+          
+
+          <Button variant="primary" size="sm" type="submit">
+            Save as Draft
+          </Button>
+
+          <Button variant="success" size="sm" type="submit">
+            Publish Blog
+          </Button>
+          </div>
+        </form>
+      </div>
   )
 }
 
