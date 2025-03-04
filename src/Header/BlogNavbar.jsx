b79b1c30be11637c67d1b6c7c01e3e2d02b6fc2d
Aksa Kurian <aksakurian55@gmail.com>
2025-03-04 10:39:20 +0530

    pp upd

--- src/Header/BlogNavbar.jsx
+++ src/Header/BlogNavbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React, { useState ,useEffect } from "react";
+import { Navbar, Nav, Container ,Dropdown} from "react-bootstrap";
 import { BsSun, BsMoon } from "react-icons/bs";
 import styles from "./Navbar.module.css"; // Import CSS module
 
@@ -7,13 +7,37 @@ import { NavLink } from "react-router-dom"; // Use NavLink instead of Link
 import DynamicModalLoginForm from "../Blog-Login/LoginForm";
 
 const BlogNavbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => 
+    localStorage.getItem("theme") === "dark");
+
+
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  
+ 
 
   const toggleTheme = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle("dark-mode");
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("theme", newMode ? "dark" : "light");
+    document.body.classList.toggle("dark-mode", newMode);
   };
 
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
   const[showLogin, setShowLogin] = useState(false);
 
   const handleShow = () => setShowLogin(true);
@@ -39,9 +63,21 @@ const BlogNavbar = () => {
             <Nav.Link onClick={toggleTheme} className={styles.ThemeToggle}>
               {darkMode ? <BsMoon size={20} /> : <BsSun size={20} />}
             </Nav.Link>
-            <Nav.Link href="#">
+            <Dropdown className="profile-dropdown">
+            <Dropdown.Toggle as="div" id="profile-dropdown" className="profile-toggle">
               <img src="https://live.staticflickr.com/65535/52211184991_eee7c06a67.jpg" alt="Profile" className={styles.ProfileImage} />
-            </Nav.Link>
+              </Dropdown.Toggle>
+             <Dropdown.Menu className="dropdown-menu-custom">
+                <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                <Dropdown.Item href="/Blog">Blogs</Dropdown.Item>
+                <Dropdown.Item href="/profile">Categories</Dropdown.Item>
+                <Dropdown.Item href="/profile">Comments</Dropdown.Item>
+                <Dropdown.Item href="/settings">Settings</Dropdown.Item>
+                <Dropdown.Item href="/logout" className="logout-item">Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            
+            
           </Nav>
         </Navbar.Collapse>
 
